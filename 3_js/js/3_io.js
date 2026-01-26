f267a19107aea9ef0afa91b2a3476e6a0b9219ad
DESKTOP-4I0Q9AM\user1 <skan64554@gmail.com>
2026-01-26 17:13:19 +0900

    CSS 마무리 및 JS 1일차

---
+++ 3_js/js/3_io.js
@@ -0,0 +1,37 @@
+confirmBtn.onclick = function(){
+    // confirm : 확인/취소버튼을 통해 논리적인 처리를 지원하는 팝업창  
+    var bool = confirm("정말 삭제하시겠습니까?");
+    console.log(bool);
+
+    if(bool) {
+        // 파일삭제 로직
+        // alert : 메세지 내용을 출력하는 팝업
+        alert("파일을 삭제했습니다.");
+    }
+};
+
+promptBtn.onclick = function(){
+    //prompt : 사용자로부터 문자열 입력을 받을 수 있는 모달
+    var name = prompt("당신의 이름은 무엇입니까?");
+
+    // 자바스크립트에서는 "값이 존재하는 경우" -> true
+    // "값이 존재하지 않는 경우" -> false로 처리
+    console.log(name); // 값이 있는 경우, 값이 없는 경우(null, "")
+    if(!name){
+    //if(name == null || name == ""){
+        alert("올바른 이름을 입력하세요")
+    }else{
+        alert("안녕하세요" + name + "님.")
+    }
+}
+
+innerH.onclick = function(){
+    /* innerHTML : 각 HTML 태그의 content 영역을 의미한다 
+        - getter 방식 : html이 포함된 content를 반환
+        - setter 방식 : 주어진 문자열을 html로 변환하여 출력
+    */
+   var str = "<h1>치킨먹고싶다</h1>";
+
+   wrapper.innerHTML = str; // setter 방식
+
+}
