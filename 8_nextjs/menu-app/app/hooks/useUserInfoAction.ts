dc58070c4c71fb9399732ef8d51d018506c2fca6
DESKTOP-4I0Q9AM\user1 <skan64554@gmail.com>
2026-04-22 17:25:21 +0900

    next 마무리

--- 8_nextjs/menu-app/app/hooks/useUserInfoAction.ts
+++ 8_nextjs/menu-app/app/hooks/useUserInfoAction.ts
@@ -1,13 +1,14 @@
 import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "../features/userInfoSlice";
+import { User } from "../type/user";
 
 export const useUserInfoActions = () => {
 
     const dispatch = useDispatch();
     const router = useRouter();
 
-    const handleLoginSync = (userData:any, accessToken?:string) => {
+    const handleLoginSync = (userData:User, accessToken?:string) => {
         dispatch(loginSuccess({
             user : userData,
             accessToken : accessToken
